17d37bd90cd78a5ac6dca016c71611fda532d412
LGH <dinggul56@gmail.com>
2023-11-16 16:11:43 +0900

    feat: 인가 설정, 로그인 확인 예정

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Register() {
@@ -54,6 +54,7 @@ function Register() {
         .catch((err) => {
           console.log(err);
         });
+      console.log('test after api');
       navigate('/login');
     }
   };
