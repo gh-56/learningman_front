1132355e05b27a093c2dffcd26c853223ad1849c
LGH <dinggul56@gmail.com>
2023-11-20 14:13:01 +0900

    이미지 데이터만 DB에 저장 (UUID 사용)

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 function Card() {
+  const baseUrl = 'http://localhost:8080';
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Create a FormData object
+    const formData = new FormData();
+    formData.append('file', file);
+
+    // You can also append other fields to the FormData object if needed
+    // formData.append('fieldName', 'fieldValue');
+
+    try {
+      // Make a POST request with Axios
+      const response = await axios.post(
+        baseUrl + '/members/profile',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+
+      // Handle the response as needed
+      console.log(response);
+    } catch (error) {
+      console.error('2:', error);
+    }
+  };
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
+      <input type='file' onChange={handleFileChange} />
+      <button type='submit'>Upload</button>
+    </form>
+    /* <div>
       <div class='card'>
         <img
           src='src/img/test_profile_img.jpg'
@@ -14,7 +55,7 @@ function Card() {
           <p class='card-text'>Card Text.</p>
         </div>
       </div>
-    </div>
+    </div>  */
   );
 }
 
