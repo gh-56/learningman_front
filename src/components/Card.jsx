ffd9fdafcf8a90472fbbbeb8fae6e68fbcd04cb6
LGH <dinggul56@gmail.com>
2023-11-24 15:07:47 +0900

    feat: 기본프로필 불러오기 및 프로필 이미지 변경 구현
    새로고침 시 변경한 프로필 이미지 유지 되는 것 구현하기

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,24 @@
 import axios from 'axios';
 import MemberInfo from '../pages/MemberInfo';
 import React, { useEffect, useState } from 'react';
+import { apiClient } from '../api/ApiClient';
+import { getCookie } from '../cookies/CookieFunction';
 
 function Card() {
   const baseUrl = 'http://localhost:8080';
   const [file, setFile] = useState(null);
   const [img, setImg] = useState(null);
+  const [baseImg, setBaseImg] = useState(null);
 
   // 파일 등록(변경)하기 => 파일 선택
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   };
-
   // 등록한 파일을 post방식으로 요청하고 응답 받음
   const handleSubmit = async (event) => {
     event.preventDefault();
-
     const formData = new FormData();
     formData.append('file', file);
-
     try {
       const response = await axios.post(
         baseUrl + '/members/profile/img',
@@ -29,13 +29,34 @@ function Card() {
           },
         }
       );
-
       console.log(response);
       setImg(response.data);
     } catch (error) {
       console.error('handleSubmit_error:', error);
     }
   };
+  const baseProfileImg = async () => {
+    try {
+      apiClient.interceptors.request.use((config) => {
+        console.log('인터셉터하여 헤더에 토큰 정보 추가');
+        config.headers.Authorization = getCookie('tokenKey');
+        return config;
+      });
+      const response = await axios.get(baseUrl + '/members/profile/baseimg', {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log('baseProfileImg: ', response.data);
+      setBaseImg(response.data);
+    } catch (error) {
+      console.error('baseProfileImg: ', error);
+    }
+  };
+
+  useEffect(() => {
+    baseProfileImg();
+  }, []);
 
   return (
     <div>
@@ -45,11 +66,19 @@ function Card() {
       </form>
 
       <div className='card' style={{ width: '18rem' }}>
-        <img
-          src={`http://localhost:8080${img}`}
-          className='card-img-top'
-          alt='프로필 이미지 없음'
-        />
+        {img !== null ? (
+          <img
+            src={`http://localhost:8080${img}`}
+            className='card-img-top'
+            alt='프로필 이미지 없음'
+          />
+        ) : (
+          <img
+            src={`http://localhost:8080${baseImg}`}
+            className='card-img-top'
+            alt='기본 프로필 이미지 없음'
+          />
+        )}
         <div className='card-body'>
           <MemberInfo />
         </div>
--- src/pages/MemberInfo.jsx
+++ src/pages/MemberInfo.jsx
@@ -1,8 +1,7 @@
-
-import { useEffect, useState } from "react";
-import { myPageApi } from "../api/ApiClient";
-import { setCookie, getCookie, removeCookie } from "../cookies/CookieFunction";
-import { apiClient, authenticateApi } from "../api/ApiClient";
+import { useEffect, useState } from 'react';
+import { myPageApi } from '../api/ApiClient';
+import { setCookie, getCookie, removeCookie } from '../cookies/CookieFunction';
+import { apiClient, authenticateApi } from '../api/ApiClient';
 
 function MemberInfo() {
   const [memberDto, setMemberDto] = useState(null);
@@ -10,8 +9,8 @@ function MemberInfo() {
   const callApi = async () => {
     // axios 인터셉터 설정 등록 : 모든 API요청에 사용된다.
     apiClient.interceptors.request.use((config) => {
-      console.log("인터셉터하여 헤더에 토큰 정보 추가");
-      config.headers.Authorization = getCookie("tokenKey");
+      console.log('인터셉터하여 헤더에 토큰 정보 추가');
+      config.headers.Authorization = getCookie('tokenKey');
       return config;
     });
 
