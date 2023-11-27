d33dd5a3f799a40bc95828c967327e6e2977fe2c
LGH <dinggul56@gmail.com>
2023-11-27 14:42:47 +0900

    머스테치와 리액트 합치기

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Header from './components/Header';
-import Card from './components/Card';
 import Home from './Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MemberInfo from './pages/MemberInfo';
 import React from 'react';
 import { AuthProvider } from './security/AuthContext';
 import HomePage from './pages/HomePage';
+import QuizRegister from './article/QuizRegister';
 
 function App() {
   return (
@@ -22,6 +22,7 @@ function App() {
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
             <Route path='/memberinfo' element={<MemberInfo />} />
+            <Route path='/articles' element={<QuizRegister />} />
           </Routes>
         </Router>
       </AuthProvider>
---
+++ src/article/QuizRegister.jsx
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+function QuizRegister() {
+  const baseUrl = 'http://localhost:8080';
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const formSubmit = async (e) => {
+    e.preventDefault();
+    await axios
+      .post(baseUrl + '/api/articles', {
+        title: title,
+        content: content,
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onChangeHandlerTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const onChangeHandlerContent = (e) => {
+    setContent(e.target.value);
+  };
+  return (
+    <div>
+      <form onSubmit={formSubmit}>
+        <div class='mb-3'>
+          <label class='form-label'>단어(한국어)</label>
+          <input
+            name='title'
+            type='text'
+            class='form-control'
+            value={title}
+            onChange={onChangeHandlerTitle}
+          />
+        </div>
+        <div class='mb-3'>
+          <label class='form-label'>단어(영어)</label>
+          <input
+            name='content'
+            type='text'
+            class='form-control'
+            value={content}
+            onChange={onChangeHandlerContent}
+          />
+        </div>
+        <button type='submit' class='btn btn-primary'>
+          단어 넣기
+        </button>
+        {/* <a href='/articles'>단어 목록</a> */}
+      </form>
+    </div>
+  );
+}
+
+export default QuizRegister;
