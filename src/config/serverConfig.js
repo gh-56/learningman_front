ba2f05f35eb6e15d4604488491a6f0d1c2b336e8
LGH <dinggul56@gmail.com>
2023-12-12 09:22:02 +0900

    local

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,4 +1,5 @@
 const serverConfig = {
-  serverUrl: 'http://ec2-43-200-163-249.ap-northeast-2.compute.amazonaws.com',
+  // serverUrl: 'http://ec2-43-200-163-249.ap-northeast-2.compute.amazonaws.com',
+  serverUrl: 'http://localhost',
 };
 export default serverConfig;
